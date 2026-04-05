d4e702f4ceddebfff48857cdce3fe330f0bd4598
Michael Jones <michael.jones90@me.com>
2026-04-05 08:25:22 +0100

    Initial Commit

---
+++ HackingWithSwift-SortVis/HackingWithSwift_SortVisApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwift_SortVisApp.swift
+//  HackingWithSwift-SortVis
+//
+//  Created by Michael Jones on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwift_SortVisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
